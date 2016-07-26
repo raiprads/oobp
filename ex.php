2838e90408a14d5048c2e03029aaa460fca1f0ed
raiprads <raiprads@gmail.com>
2016-07-27 02:11:28 +0300

    Namespacing and Autoloading

--- ex.php
+++ ex.php
@@ -1,53 +1,8 @@
 <?php 
 
-
-class Person
-{
-	protected $name;
-
-	public function __construct($name)
-	{
-		$this->name = $name;
-	}
-}
-
-class Business
-{
-	public function __construct(Staff $staff)
-	{
-		$this->staff = $staff;
-	}
-
-	public function hire(Person $person)
-	{
-		$this->staff->add($person);
-	}
-
-	public function getStaffMembers()
-	{
-		return $this->staff->members();
-	}
-}
-
-class Staff
-{
-	protected $members = [];
-
-	public function __construct($members = [])
-	{
-		$this->members = $members;
-	}
-
-	public function add(Person $person)
-	{
-		$this->members[] = $person;	
-	}
-
-	public function members()
-	{
-		return $this->members;
-	}
-}
+use Acme\Users\Person;
+use Acme\Business;
+use Acme\Staff;
 
 $ryan = new Person('Ryan Prader');
 
